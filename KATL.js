fd6cf7017c3eebe5b721d4e6e94a7e771d7dc31f
TemporalFuzz <vanxmax@gmail.com>
2016-12-31 12:29:26 -0800

    Update KATL.js

--- KATL.js
+++ KATL.js
@@ -25,7 +25,7 @@ var Kit = {
   M: function(v1, v2) {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Params v1, v2 (Vector Arrays): Two points on the line to find the slope of
-     * @Returns: The slope of the line that passes through v1 and v2
+     * @Returns (Number): The slope of the line that passes through v1 and v2
      * @Revisions: None
     */
     return (v1[1] - v2[1])/(v1[0] - v2[0]);
@@ -33,7 +33,7 @@ var Kit = {
   B: function(v1, v2) {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Params v1, v2 (Vector Arrays): Two points on the line to find the y intercept of
-     * @Returns: The y intercept of the line that passes through v1 and v2
+     * @Returns (Number): The y intercept of the line that passes through v1 and v2
      * @Revisions: None
     */
     return v1[1] - M(v1, v2) * v1[0];
@@ -41,7 +41,7 @@ var Kit = {
   PM: function(v1, v2) {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Params v1, v2 (Vector Arrays): Two points on the line to find the perpendicular slope of
-     * @Returns: The slope of any line perpendicular to the line that passes through v1 and v2
+     * @Returns (Number): The slope of any line perpendicular to the line that passes through v1 and v2
      * @Revisions: None
     */
     return -1/M(v1, v2);
@@ -50,7 +50,7 @@ var Kit = {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Param v (Vector Array): A point that the line passes through
      * @Param m (Number): The slope of the line
-     * @Returns: The y intercept of the line that passes through v and has slope m
+     * @Returns (Number): The y intercept of the line that passes through v and has slope m
      * @Revisions: None
     */
     return v[1] - (m * v[0]);
@@ -58,7 +58,7 @@ var Kit = {
   vectAdd: function(v1, v2) {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Params v1, v2 (Vector Arrays): The two vectors to be added.
-     * @Returns: The vector addition of vectors v1 and v2
+     * @Returns (Vector Array): The vector addition of vectors v1 and v2
      * @Revisions: None
     */
     return [v1[0] + v2[0], v1[1] + v2[1]];
@@ -67,7 +67,7 @@ var Kit = {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Param v (Vector Array): The original vector to be subtracted from
      * @Param s (Vector Array): The vector that will be subtracted from v
-     * @Returns: The vector subtraction of v minus s
+     * @Returns (Vector Array): The vector subtraction of v minus s
      * @Revisions: None
     */
     return [v[0] - s[0], v[1] - s[1]];
@@ -76,7 +76,7 @@ var Kit = {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Param v (Vector Array): The original vector to be multiplied
      * @Param d (Number): The number that the vector is to be multiplied by.
-     * @Returns: The vector multiplication of v times m
+     * @Returns (Vector Array): The vector multiplication of v times m
      * @Revisions: None
     */
     return [v[0] * m, v[1] * m];
@@ -85,7 +85,7 @@ var Kit = {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Param v (Vector Array): The original vector to be divided
      * @Param d (Number): The number that the vector is to be divided by.
-     * @Returns: The vector division of v over d
+     * @Returns (Vector Array): The vector division of v over d
      * @Revisions: None
     */
     return [v[0]/d, v[1]/d];
@@ -93,7 +93,7 @@ var Kit = {
   vectMag: function(v) {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Param v (Vector Array): The vector to find the magnitude of
-     * @Returns: The magnitude of v
+     * @Returns (Vector Array): The magnitude of v
      * @Revisions: None
     */
     return sqrt(v[0] * v[0] + v[1] * v[1]);
@@ -101,16 +101,16 @@ var Kit = {
   vectNorm: function(v) {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Param v (Vector Array): The vector to be normalized
-     * @Returns: The normalized form of v
+     * @Returns (Vector Array): The normalized form of v
      * @Revisions: None
     */
-    return TK.vectDiv(v, TK.vectMag(v));
+    return Kit.vectDiv(v, Kit.vectMag(v));
   },
   vectRot: function(v, t) {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Param v (Vector Array): The vector to be rotated
      * @Param t (Number): How many degrees/radians to rotate the vector by
-     * @Returns: The rotated form of v by t degrees/radians
+     * @Returns (Vector Array): The rotated form of v by t degrees/radians
      * @Revisions: None
     */
     return [v[0] * cos(t) - v[1] * sin(t), v[1] * cos(t) + v[0] * sin(t)];
@@ -118,7 +118,7 @@ var Kit = {
   vectMid: function(v1, v2) {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Params v1, v2 (Vector Arrays): The endpoints of the line to find the midpoint of
-     * @Returns: The point exactly between v1 and v2
+     * @Returns (Vector Array): The point exactly between v1 and v2
      * @Revisions: None
     */
     return [v1[0]/2 + v2[0]/2, v1[1]/2 + v2[1]/2];
@@ -126,15 +126,15 @@ var Kit = {
   vectDist: function(v1, v2) {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Params v1, v2 (Vector Arrays): The points to find the distance between
-     * @Returns: The distance between v1 and v2
+     * @Returns (Number): The distance between v1 and v2
      * @Revisions: None
     */
-    return TK.vectMag(TK.vectSub(v1, v2));
+    return Kit.vectMag(Kit.vectSub(v1, v2));
   },
   vectHead: function(v) {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Param v (Vector Array): The vector to find the heading of
-     * @Returns: The heading of v
+     * @Returns (Number): The heading of v
      * @Revisions: None
     */
     return atan2(v[1], v[0]);
@@ -143,14 +143,37 @@ var Kit = {
     /* @Author: TemporalFuzz (@maxzman14)
      * @Param v (Vector Array): The vector to be reflected
      * @Params a, b (Vector Arrays): Two points on the line that v will be reflected over
-     * @Returns: v reflected over the line that passes through both a and b
+     * @Returns (Vector Array): v reflected over the line that passes through both a and b
      * @Revisions:  
        * Uses variables rather than calling functions multiple times (12/31/16, @maxzman14)
     */
-    var MAB = TK.M(a, b);
-    var BAB = TK.B(a, b);
-    var PMAB = TK.PM(a, b);
-    var TBABV = TK.TB(PMAB, v);
-    return TK.vectSub(v, TK.vectMult(TK.vectSub(v, [(BAB - TBABV)/(PMAB - MAB), MAB * (BAB - TBABV)/(PMAB - MAB) + BAB]), 2));         
+    var MAB = Kit.M(a, b);
+    var BAB = Kit.B(a, b);
+    var PMAB = Kit.PM(a, b);
+    var TBABV = Kit.TB(PMAB, v);
+    return Kit.vectSub(v, Kit.vectMult(Kit.vectSub(v, [(BAB - TBABV)/(PMAB - MAB), MAB * (BAB - TBABV)/(PMAB - MAB) + BAB]), 2));         
+  },
+  colliding: {
+    /* This contains all collision check functions between shapes */
+    circleCircle: function(p1, r1, p2, r2) {
+      /* @Author: TemporalFuzz (@maxzman14)
+       * @Params p1, p2 (Vector Arrays): The positions of the two circles
+       * @Params r1, r2 (Numbers): The radii of each circle
+       * @Returns (Boolean): Whether the two given circles are colliding
+       * @Revisions: None
+      */
+      return Kit.vectDist(p1, p2) < r1 + r2;
+    },
+    rectRect: function(x1, y1, w1, h1, x2, y2, w2, h2) {
+      /* @Author: TemporalFuzz (@maxzman14)
+       * @Params x1, x2 (Numbers): The x positions of the center of each rectangle
+       * @Params y1, y2 (Numbers): The y positions of the center of each rectangle
+       * @Params w1, w2 (Numbers): The widths of each rectangle
+       * @Params h1, h2 (Numbers): The heights of each rectangle
+       * @Returns (Boolean): Whether the two rectangles are colliding
+       * @Revisions: None
+      */
+      return abs(x1 - x2) * 2 < w1 + w2 && abs(y1 - y2) * 2 < h1 + h2;
+    }
   }
 };
